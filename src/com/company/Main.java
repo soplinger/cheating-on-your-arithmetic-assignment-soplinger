9265b2676bb433d0e4ef0d249512f98d2ec91ecb
soplinger <solekibbles4@gmail.com>
2018-09-05 22:52:26 -0400

    i told my dog he was going to summer camp, little did he know it was the coal mines

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Tester boys = new Tester();
+
+        System.out.println("79 + 3 * (4+ 82 - 68) - 7 + 19 = " + boys.boy());
+        System.out.println("(179 + 21 + 10) /7 + 181) = " + boys.boyo());
+        System.out.println("10359 * 56 * 11 + 2246 = " + boys.boyus());
+    }
+}
---
+++ src/com/company/Tester.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Tester {
+
+    public int boy() {
+
+
+        return 79 + 3 * (4 + 82 - 68) - 7 + 19;
+    }
+        public int boyo(){
+
+
+            return (179+21+10)/7+181;}
+
+
+
+            public int boyus(){
+
+
+                return 10359*56*11+2246;
+
+
+
+
+
+            }
+}
